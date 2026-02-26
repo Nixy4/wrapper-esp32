38ccc835223a2d4f676babb04ea3237ece82eef5
Nixy4 <609610903@qq.com>
2026-02-27 04:00:43 +0800

    save

--- dist/wrapper-esp32_0.1.1/src/device/ili9341.cpp
+++
@@ -1,9 +0,0 @@
-#include "ili9341.hpp"
-
-namespace wrapper {
-
-bool Ili9341::Init(const SpiBus& bus, const SpiDisplayConfig& config) {
-    return SpiDisplay::Init(bus, config, esp_lcd_new_panel_ili9341);
-}
-
-} // namespace wrapper
